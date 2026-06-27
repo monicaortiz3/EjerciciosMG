f4fd06b16f3f8b87a77ae1cdb262ac1a04f268fb
monicaortiz3 <ortizmonicadamaris@gmail.com>
2026-06-27 12:33:37 -0600

    Se sube modificacion del JS

--- ForntEjercicio/app.js
+++ ForntEjercicio/app.js
@@ -1,7 +1,18 @@
-fetch("http://localhost:8081/api/v1/fecha-hora-actual")
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("fecha").textContent = data.fecha;
-        document.getElementById("hora").textContent = data.hora;
-    })
-    .catch(error => console.error("Error:", error));
+const fecha = document.getElementById('fecha');
+const hora = document.getElementById('hora');
+
+async function fechaHoraActual(fecha, hora) {
+    console.log('Entro');
+    try {
+        const response = await fetch('http://localhost:8081/api/v1/fecha-hora-actual');
+
+        const data = await response.json();
+
+        fecha.textContent = data.fecha;
+        hora.textContent = data.hora;
+    }
+    catch (error) {
+        console.error('Error al obtener la fecha y hora actual:', error);
+    }
+}
+fechaHoraActual(fecha, hora);
